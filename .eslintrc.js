f06d6aab48b7d06343494852866f69b78fd25d09
NekOrz <joe1256joe@gmail.com>
2016-12-25 23:31:27 -0800

    Starter
    Initialize some basic file.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"extends": "airbnb",
+	"plugins": [
+		"react",
+		"jsx-a11y",
+		"import"
+	],
+	"rules": {
+		"brace-style": ["error", "stroustrup", {"allowSingleLine": true}],
+		"no-console": 0
+	}
+};
