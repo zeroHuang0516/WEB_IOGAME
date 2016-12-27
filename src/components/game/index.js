fb444d291316fd6875e8aa778540f7c2f8aa676b
NekOrz <joe1256joe@gmail.com>
2016-12-27 20:02:38 +0800

    CSS fixed
    Now it looks good

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -3,18 +3,20 @@ import React, { Component } from 'react';
 class Screen extends Component {
   render() {
     return (
-      <div className="screen">screen
-        <span className="left">left
-          <div className="building">building</div>
-          <div className="resource">resource</div>
-        </span>
-        <span className="right">right
-          <div className="map">map</div>
-          <div className="info">info
-            <span className="detail">detail</span>
-            <span className="minimap">minimap </span>
-          </div>
-        </span>
+      <div className="container-fluid">
+        <div className="row screen">
+          <span className="col-md-2 left">
+            <div className="row building">building</div>
+            <div className="row resource">resource</div>
+          </span>
+          <span className="col-md-10 right">
+            <div className="row map">map</div>
+            <div className="row info">
+              <span className="col-md-10 detail">detail</span>
+              <span className="col-md-2 minimap">minimap</span>
+            </div>
+          </span>
+        </div>
       </div>
     );
   }
